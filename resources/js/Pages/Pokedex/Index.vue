2cda0392740b53f2d805203f2ba54e48c2cbac81
hososo <hoso6616@gmail.com>
2023-06-01 14:04:23 +0900

    index画面レイアウト修正

--- resources/js/Pages/Pokedex/Index.vue
+++ resources/js/Pages/Pokedex/Index.vue
@@ -3,6 +3,7 @@ import { onMounted } from "vue";
 import { Link } from "@inertiajs/vue3";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+// import '/css/item.css';
 onMounted(() => {
     // console.log(data);
 });
@@ -13,29 +14,35 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="ポケモン図鑑" />
     <AuthenticatedLayout>
-        <div class="py-4 bg-blue-200">
+        <template #header>
+            <div class="flex justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">ポケモン図鑑</h2>
+                <div class="flex">
+                    <h2>表示順ボタンを置きたい</h2>
+                    <h2>検索ボタンを置きたい</h2>
+                </div>
+            </div>
+        </template>
+        <div class="py-4 bg-blue-100">
             <div class="mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg  bg-blue-200">
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg  bg-blue-100">
                     <div class="p-6 text-gray-900">
                         <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-                            <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                            <!-- <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
                                 <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">ポケモン図鑑</h2>
-                            </div>
-                            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 md:gap-12">
-                                <div v-for="poke in pokeinfo" class="text-center  bg-white rounded-xl">
+                            </div> -->
+                            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 md:gap-12">
+                                <div v-for="poke in pokeinfo" class="text-center  bg-white rounded-xl border border-red-500 transform hover:scale-110 transition-transform">
                                     <Link :href="route('pokedex.show', { pokedex: poke['p_id'] })">
-                                    <img class="rounded-xl sm:w-48 sm:h-48 lg:w-60 lg:h-60 mx-auto" :src="poke['front_default']">
+                                    <img class="rounded-xl sm:w-48 sm:h-48 lg:w-50 lg:h-50 mx-auto" :src="poke['front_default']">
                                     </Link>
                                     <div class="mt-2 sm:mt-4">
                                         <h3 class="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-gray-200">
-                                        {{ poke['p_id'] }}
-                                        {{ poke['jp_name'] }}
-                                        </h3>
-                                        <p class="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-gray-400">
-                                          Founder / CEO
-                                        </p>
+                                            {{ poke['p_id'] }}
+                                            {{ poke['jp_name'] }}
+                                            </h3>
                                     </div>
                                 </div>
                             </div>
